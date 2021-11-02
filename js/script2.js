dd942ff810dc360501af8544ac5b35f84b9d9848
Sojer <sojer90@gmail.com>
2021-11-02 21:19:12 +0200

    homehork with recomendation

---
+++ js/script2.js
@@ -0,0 +1,19 @@
+let lightColor = prompt('Please put light color of traffic lights', 'green');
+
+function trafficLights(lightColor) {
+    if (lightColor >= 0 ) {
+        return 'We need a light of traffic lights';
+    }
+    switch (lightColor.toLowerCase()) {
+        case 'green':
+            return 'yellow';
+        case 'yellow':
+            return 'red';
+        case 'red':
+            return 'green';
+        default:
+            return 'It`s wrong value -' + ' ' + lightColor;
+    }
+}
+
+alert(trafficLights(lightColor));
---
+++ js/script3.js
@@ -0,0 +1,19 @@
+let width = +prompt('Please enter width of polygon','5');
+let length = +prompt('Please enter length of polygon','6');
+
+function polygon(width,length) {
+    if ((isNaN(width && length))) {
+        return 'We need numbers dude';
+    }
+    if (width === length) {
+        const area = 'Area of square' + ' ' + width ** 2;
+        return area;
+    }
+    else {
+        const perimeter = 'Perimeter og rectangle'+ ' ' + (width + length) * 2;
+        return perimeter;
+    }
+}
+
+alert(polygon(width,length));
+
